14cb96656bd7fe874696559960c79d09af669eec
Sorcha Abel <Sorcha.Abel@htw.com.au>
2015-12-18 12:39:05 +1000

    udemy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogApp::Application.config.secret_key_base = '10dc75f175fcf2d74778ebde7fd1c7998370010dbc85fb050136c8d8a4927ea3d92c7994fe668234a7271addc9df35d6a8dc4799d17c3f4036ac2ab6059ea8f9'
